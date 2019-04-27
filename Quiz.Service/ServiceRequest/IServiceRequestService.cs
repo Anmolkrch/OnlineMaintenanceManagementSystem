3a5106869436427df65b4bc5ded9d9d5cb1cf39d
Brajesh Kumar <Brajeshkr18@gmail.com>
2019-04-27 20:10:05 +0530

    initial commit

---
+++ Quiz.Service/ServiceRequest/IServiceRequestService.cs
@@ -0,0 +1,19 @@
+﻿using Quiz.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Service.ServiceRequest
+{
+    public interface IServiceRequestService
+    {
+
+        dynamic ServiceRequestsList();
+        dynamic serviceRequest(int? id);
+        bool SaveServiceRequests(ServiceRequestViewModel atten);
+        bool DeleteserviceRequest(int id);
+
+    }
+}
